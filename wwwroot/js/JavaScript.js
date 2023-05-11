1ab0126270ea4ba9f475356ddb8f7836b8c30f69
tanaya167 <tanaya.lai@students.plymouth.ac.uk>
2023-05-12 00:37:02 +0100

    User login

--- wwwroot/js/JavaScript.js
+++ wwwroot/js/JavaScript.js
@@ -1,8 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your Javascript code.
-$(document).ready(function () {
+﻿$(document).ready(function () {
     // hide dropdown menus on page load
     $('.dropdown-menu').hide();
 
